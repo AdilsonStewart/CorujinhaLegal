683ae77c804cf60e227821ccd2448e0cf03cecf4
AdilsonStewart <144806733+AdilsonStewart@users.noreply.github.com>
2025-12-12 16:20:21 -0300

    Fix storageBucket URL in firebase config
    Updated storageBucket URL to the correct format.

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyASmPjNdBFly7ndXk0n-FFbWT-2DQLlevI",
   authDomain: "corujinhalegal2-5c7c9.firebaseapp.com",
   projectId: "corujinhalegal2-5c7c9",
-  storageBucket: "corujinhalegal2-5c7c9.firebasestorage.app",
+  storageBucket: "corujinhalegal2-5c7c9.appspot.com",
   messagingSenderId: "711736746096",
   appId: "1:711736746096:web:dd3a64784367133dd414b5"
 };
