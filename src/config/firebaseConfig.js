f0bfc82617472b4d90c1710550060991076824b7
AdilsonStewart <144806733+AdilsonStewart@users.noreply.github.com>
2025-12-12 17:29:40 -0300

    Add Firebase configuration and initialization

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,19 @@
+// firebaseConfig.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_AUTH_DOMAIN",
+  projectId: "SEU_PROJECT_ID",
+  storageBucket: "SEU_STORAGE_BUCKET",
+  messagingSenderId: "SEU_SENDER_ID",
+  appId: "SEU_APP_ID"
+};
+
+// Inicializa Firebase
+const app = initializeApp(firebaseConfig);
+
+// Exporta Firestore
+export const db = getFirestore(app);
+export default app;
