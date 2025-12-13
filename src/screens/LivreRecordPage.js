685e5498a30dc2160bb16d6c2082ed9859b4aea8
AdilsonStewart <144806733+AdilsonStewart@users.noreply.github.com>
2025-12-13 11:09:54 -0300

    Create LivreRecordPage component
    Initial implementation of LivreRecordPage component.

---
+++ src/screens/LivreRecordPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function LivreRecordPage() {
+  return (
+    <div style={{ padding: 20, textAlign: "center" }}>
+      <h2>📽️ Gravação Livre</h2>
+      <p>Página ainda não implementada.</p>
+    </div>
+  );
+}
