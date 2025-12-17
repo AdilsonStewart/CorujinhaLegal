0991420cad6c15acb34ac2ec413b5e0294e5d787
AdilsonStewart <144806733+AdilsonStewart@users.noreply.github.com>
2025-12-17 15:22:28 -0300

    Refactor index.js to streamline owlrunner import
    Removed comments and simplified exports in index.js.

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,4 @@
-const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-
 admin.initializeApp();
 
-/**
- * Robô oficial de envio
- * Toda a lógica está em owlrunner.js
- */
-const { owlrunner } = require('./owlrunner');
-
-exports.owlrunner = owlrunner;
+require('./owlrunner');
