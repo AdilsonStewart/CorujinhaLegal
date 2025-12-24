5ef768bc4a875b8fe8c2b309aa42cf7cb07fc4c5
AdilsonStewart <144806733+AdilsonStewart@users.noreply.github.com>
2025-12-24 11:38:45 -0300

    Add logo and update button comment in Home.js
    Added logo to the top of the Home screen and updated button comment.

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -29,6 +29,15 @@ export default function Home() {
 
   return (
     <div className="container">
+      {/* LOGO OWLBUDDY NO TOPO - NOVO! */}
+      <div style={{ textAlign: 'center', marginBottom: '20px', marginTop: '20px' }}>
+        <img
+          src="https://lh3.googleusercontent.com/pw/AP1GczN8z3zV6o3bL9kQeXhV0pR7sT2uV4wY6z8A0B2C4D6E8F0G2H4I6J8K0L2M=w800-h600"  // Link direto do seu logo (ajustado pra tamanho bom)
+          alt="Logo OwlBuddy"
+          style={{ maxWidth: '80%', height: 'auto', borderRadius: '10px' }}  // Responsivo e bonito
+        />
+      </div>
+
       <p className="nao-esqueca">Não Esqueça Mais:</p>
       <div className="mascote-container">
         <img
@@ -42,7 +51,7 @@ export default function Home() {
       <h1 className="titulo">DeixaComigo</h1>
       <p className="slogan">Lembrou agora?<br />Programe o parabéns!</p>
 
-      {/* Botão principal: vai direto para a tela de identificação (criar lembrete) */}
+      {/* Botão principal */}
       <button
         className="botao criar-lembrete"
         onClick={() => navigate('/sou-cliente')}
